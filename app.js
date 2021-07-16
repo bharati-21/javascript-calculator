d96f349d227ee531baf8f4287f3b59ce5bb55fdd
Bharati Subramanian <bharatisharada@gmail.com>
2021-07-16 10:11:11 +0530

    feat: remove previous expression
    Remove previous expression on a button  click (number or operartor)

--- app.js
+++ app.js
@@ -18,46 +18,51 @@ function handleButtonClick(e) {
     const clickTarget = e.target;
     const targetClassList = clickTarget.classList;
 
+
     if(targetClassList.contains('btn')) {
         // Get the text of button
-
         const btnText = clickTarget.textContent;
 
+        // Check if there is the result of a previous expression
+
+        checkPreviousExpression(btnText);
+
         // Check if the button was an operator 
+        if(targetClassList.contains('btn-op')) {
+
+            // if(checkPreviousExpression(btnText))
+
             // 1) Check if the number is "="
                 // 1.1) Equal was pressed without an expression
                 // 1.2) Equal pressed after an operand
-            
-            if(targetClassList.contains('btn-op')) {
-                console.log('You pressed an operator');
-                if(btnText === '=') {
-                    if(validExpression()) {
-                        expression+=currentResultValue;
-                        console.log(expression);
-                        showExpression();
-                        showResult();
-                    }  
-                }
 
-                else {
-                    if(checkIfLastCharIsOPerator()) {
-                        expression = expression.slice(0,expression.length-3);
-                    }
-                    else {
-                        console.log('you pressed', btnText);
-
-                        expression+= currentResultValue + ` ${btnText} `;
-                        console.log(expression);
-                        showExpression();
-
-                    }
-                }
+            if(btnText === '=') {
+                if(validExpression()) {
+                    expression+=currentResultValue;
+                    showResult();
+                    expression+=" =";
+                    showExpression();
+                }  
             }
 
+            else {
+                if(checkIfLastCharIsOPerator()) {
+                    expression = expression.slice(0,expression.length-3);
+                }
+                else {
+                    expression+= currentResultValue + ` ${btnText} `;
+                    showExpression();
+                    clearResult();
+                    clearCurrentResultValue();
+                }
+            }
+        }
+
         // Check if the button was an operand (number or period) 
-        if(targetClassList.contains('btn-num')) {
+        else if (targetClassList.contains('btn-num')) {
             // Check if the button is a period
-            
+            checkPreviousExpression(btnText);
+
             if(btnText === '.') {
                 // Check if the period has already been clicked
                 if(!periodInOperand()) {
@@ -76,7 +81,6 @@ function handleButtonClick(e) {
                 else {
                     if(currentResultValue !== '0') {
                         currentResultValue+=btnText;
-                        console.log(currentResultValue);
                     }
                 }
             }
@@ -93,7 +97,7 @@ function handleButtonClick(e) {
             // 3. Remove the last character
 
         // Check if the button was reset
-        if(targetClassList.contains('btn-reset')) {
+        else if(targetClassList.contains('btn-reset')) {
             clearDisplay();
             clearExpression();
             clearResult();
@@ -108,7 +112,7 @@ function clearDisplay() {
 }
 
 function clearResult() {
-    divResult.textContent = "";
+    divResult.textContent = "0";
 }
 
 function clearExpression() {
@@ -116,7 +120,7 @@ function clearExpression() {
 }
 
 function clearCurrentResultValue() {
-    currentResultValue = "";
+    currentResultValue = "0";
 }
 
 function periodInOperand() {
@@ -131,17 +135,14 @@ function displayOperand() {
 }
 
 function validExpression() {
-    console.log("validExpression");
     if(checkIfLastCharIsOPerator()) {
-        console.log("checkIfLastCharIsOperator");
         return false;
         
     }
 
     const currValueChar = currentResultValue[currentResultValue.length-1];
-    if(currValueChar === '.') {
+    if(currValueChar === '.') { 
         currentResultValue = currentResultValue.slice(0, currentResultValue.length-1);
-        console.log(currentResultValue);
     }
 
     return true;
@@ -159,11 +160,26 @@ function checkIfLastCharIsOPerator() {
 
 function showExpression() {
     divDisplay.textContent = expression;
-    divResult.textContent = "0";
-    currentResultValue = "0";
 }
 
 function showResult() {
     divResult.textContent = eval(expression);
-
 }
+
+function checkPreviousExpression(btnText) {
+    if(expression.indexOf("=")!==-1) {
+        if(btnText === '=') {
+            expression = expression.slice(0, expression.length-2);
+            currentResultValue = "";
+            return;
+        }
+        clearPreviousExpression(); 
+    }
+}
+
+function clearPreviousExpression() {
+    clearExpression();
+    clearCurrentResultValue();
+    clearResult();
+    clearDisplay();
+} 
